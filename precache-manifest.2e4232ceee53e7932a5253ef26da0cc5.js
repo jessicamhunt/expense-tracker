ee227ba7cce57599d49a79409ad072eafeffb3b8
jessicamhunt <j.m.hunt1018@gmail.com>
2020-09-30 17:24:15 -0400

    Updates

---
+++ precache-manifest.2e4232ceee53e7932a5253ef26da0cc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "039f9098ad5ffd9a7d898a0b637fd696",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "269f61b834fa9c81c26b",
+    "url": "/expense-tracker/static/css/main.f862db43.chunk.css"
+  },
+  {
+    "revision": "64629b05b432c44e1bd6",
+    "url": "/expense-tracker/static/js/2.4599387a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-tracker/static/js/2.4599387a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "269f61b834fa9c81c26b",
+    "url": "/expense-tracker/static/js/main.0fcc494f.chunk.js"
+  },
+  {
+    "revision": "7a228fdbfe8715a85308",
+    "url": "/expense-tracker/static/js/runtime-main.91d22b83.js"
+  },
+  {
+    "revision": "01e7c217f689ae4e8fdcbd54c2e661da",
+    "url": "/expense-tracker/static/media/exptrack.01e7c217.jpg"
+  }
+]);
